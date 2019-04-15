ba4bacab39d5f88fdd9c49dcc82bdc51f53752e3
umairx97 <umair.bajwa@hotmail.com>
2019-04-15 22:55:01 +0500

    Created News Functionality

---
+++ app.js
@@ -0,0 +1,29 @@
+const yargs = require("yargs");
+const inquirer = require("inquirer");
+const figlet = require("figlet");
+const validator = require("validator");
+const chalk = require("chalk");
+const fs = require("fs");
+
+const { getWeather, getNews, getRegion } = require("./helper");
+
+yargs.command({
+  command: "location",
+  describe: "This command is to write your location",
+  builder: {
+    country: {
+      describe: "Location to search",
+      alias: "l",
+      demandOption: true,
+      type: "string"
+    }
+  },
+
+  handler: argv => {
+    getWeather(argv.country);
+    getNews(argv.country);
+    getRegion(argv.country);
+  }
+});
+
+yargs.parse();
