4e619b07cabb880d4e8792191fcaebf828e15456
umairx97 <umair.bajwa@hotmail.com>
2019-04-15 23:57:36 +0500

    Work

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const yargs = require("yargs");
-const inquirer = require("inquirer");
-const figlet = require("figlet");
-const validator = require("validator");
-const chalk = require("chalk");
-const fs = require("fs");
+// const yargs = require("yargs");
+// const inquirer = require("inquirer");
+// const figlet = require("figlet");
+// const validator = require("validator");
+// const chalk = require("chalk");
+// const fs = require("fs");
 
-const { getWeather, getNews, getRegion } = require("./helper");
+// const { getWeather, getNews, getRegion } = require("./helper");
 
-yargs.command({
-  command: "location",
-  describe: "This command is to write your location",
-  builder: {
-    country: {
-      describe: "Location to search",
-      alias: "l",
-      demandOption: true,
-      type: "string"
-    }
-  },
+// yargs.command({
+//   command: "location",
+//   describe: "This command is to write your location",
+//   builder: {
+//     country: {
+//       describe: "Location to search",
+//       alias: "l",
+//       demandOption: true,
+//       type: "string"
+//     }
+//   },
 
-  handler: argv => {
-    getWeather(argv.country);
-    getNews(argv.country);
-    getRegion(argv.country);
-  }
-});
+//   handler: argv => {
+//     getWeather(argv.country);
+//     getNews(argv.country);
+//     getRegion(argv.country);
+//   }
+// });
 
-yargs.parse();
+// yargs.parse();
+
+cost
