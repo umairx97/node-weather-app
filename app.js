78fea6198ef430bf2850a9cbaf034d629dff1fcf
umairx97 <umair.bajwa@hotmail.com>
2019-04-16 19:48:10 +0500

    Complete

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const request = require("request");
 const chalk = require("chalk");
 const city = process.argv[2];
+// const oldKey = 'san4GqjWsND0tGapYRWyB1knt8XtgkgP'
+// const newKey = "IkctBCdqSDrUh33Pz0eOwfhxBTknTaAV"
 
 // Returns 4 news from the country name given
 const getNews = countryId => {
   const url = `https://newsapi.org/v2/everything?q=${countryId}?&apiKey=c0a3992f469a42e1a0533456c6dd4919`;
-  request(web, (error, response, body) => {
+  request(url, (error, response, body) => {
     const info = JSON.parse(body);
-    for (let i = 0; i <= 3; i++) {
-      console.log(chalk.red("Author: "), info.articles[i].author);
-      console.log(chalk.green("Title: "), info.articles[i].title);
+
+    console.log("\t \t \t " + chalk.cyan.bold("News for " + countryId));
+    for (let i = 0; i <= 4; i++) {
+      console.log(chalk.cyan("Author: "), info.articles[i].author);
+      console.log(chalk.yellow("Title: "), info.articles[i].title);
       console.log("\n");
     }
   });
@@ -17,29 +21,61 @@ const getNews = countryId => {
 
 // Returns the country name and province based on city key provided
 const getCountry = cityKey => {
-  const url = `http://dataservice.accuweather.com/locations/v1/${cityKey}?apikey=san4GqjWsND0tGapYRWyB1knt8XtgkgP`;
+  const url = `http://dataservice.accuweather.com/locations/v1/${cityKey}?apikey=IkctBCdqSDrUh33Pz0eOwfhxBTknTaAV`;
   request(url, (err, res, body) => {
     const info = JSON.parse(body);
-    console.log(chalk.green("Country Name: "), info.Country.LocalizedName);
-    console.log(chalk.green("Province Name: "), info.Province.LocalizedName);
+    console.log("\t \t \t " + chalk.cyan.bold("Region:"));
+    console.log(chalk.yellow("Country Name: "), info.Country.LocalizedName);
+    console.log(
+      chalk.yellow("Province Name: "),
+      info.AdministrativeArea.LocalizedName
+    );
     // Passing the country name to get the news
     getNews(info.Country.LocalizedName);
   });
 };
 
-// Returns a key for a specific city to get the country
-getCity = city => {
-  const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=san4GqjWsND0tGapYRWyB1knt8XtgkgP&q=${city}`;
+// Returns the weather against a city key
+const getWeather = cityKey => {
+  const url = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${cityKey}?apikey=IkctBCdqSDrUh33Pz0eOwfhxBTknTaAV&metric=true`;
+
   request(url, (error, response, body) => {
-    console.log(response);
-    const data = JSON.parse(body);
-    for (let i = 0; i < data.length; i++) {
-      getCountry(data[i].Key);
-      break;
-    }
+    const info = JSON.parse(body);
+    const forecast = info.DailyForecasts;
+
+    console.log("\t \t \t " + chalk.cyan.bold("Weather:"));
+    forecast.map(item =>
+      console.log(
+        "Minimum: " +
+          item.Temperature.Minimum.Value +
+          item.Temperature.Minimum.Unit
+      )
+    );
+    forecast.map(item =>
+      console.log(
+        "Maximum: " +
+          item.Temperature.Maximum.Value +
+          item.Temperature.Maximum.Unit
+      )
+    );
   });
 };
 
-// getCity(city);
+// Returns a key for a specific city to get the country
+getCity = city => {
+  if (!city) {
+    console.log(chalk.bgRed("Please Enter A City Name"));
+  } else {
+    const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=IkctBCdqSDrUh33Pz0eOwfhxBTknTaAV&q=${city}`;
+    request(url, (error, response, body) => {
+      const data = JSON.parse(body);
+      for (let i = 0; i < data.length; i++) {
+        getCountry(data[i].Key);
+        getWeather(data[i].Key);
+        break;
+      }
+    });
+  }
+};
 
-getNews("india");
+getCity(city);
